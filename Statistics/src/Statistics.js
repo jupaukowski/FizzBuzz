f897a9eb3d9cf50fb31969f14219657bf0275cf2
Juliana <jpaukows@thoughtworks.com>
2016-03-01 14:28:53 -0300

    Statistic code w/o test

--- Statistics/src/Statistics.js
+++ Statistics/src/Statistics.js
@@ -1,8 +1,16 @@
 function statistics (sequence[]) {
-	var minimumValue = 0;
-	var maximumValue = 0;
+	var minimumValue = sequence[0];
+	var maximumValue = sequence[0];
 	var sequenceLength = sequence.length;
 	var averageValue = 0;
 
-	
+	for (i = 0; i < sequenceLength; i++) {
+		averageValue += sequence[i];
+		if (minimumValue < sequence[i]) {
+			minimumValue = sequence[i];
+		} else if (maximumValue > sequence[i] {
+			maximumValue = sequence[i];
+		} 
+	}
+	averageValue /= sequenceLength;
 }
