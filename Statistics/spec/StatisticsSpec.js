aa002e4f56be9b2ba8c9875c61e5f89cc58ce76e
Juliana <jpaukows@thoughtworks.com>
2016-03-01 11:32:58 -0300

    New Kata Statistics

---
+++ Statistics/spec/StatisticsSpec.js
@@ -0,0 +1,8 @@
+var statistics = require("../src/Statistics.js")
+
+describe("Statistics", function () {
+
+	it("see if it works", function (){
+		expect(true).toBe(true);
+	}
+}
---
+++ Statistics/src/Statistics.js
@@ -0,0 +1,8 @@
+function statistics (sequence[]) {
+	var minimumValue = 0;
+	var maximumValue = 0;
+	var sequenceLength = sequence.length;
+	var averageValue = 0;
+
+	
+}
