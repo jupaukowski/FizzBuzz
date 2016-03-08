789e75c20be8d3048db9f93adf61c9dada0f46db
Juliana <jpaukows@thoughtworks.com>
2016-03-07 22:40:01 -0300

    Statistic kata in one file

--- Statistics/spec/StatisticsSpec.js
+++ Statistics/spec/StatisticsSpec.js
@@ -1,29 +1,17 @@
-var findMinimumValue = require("../src/Statistics.js")
-var findMaximumValue = require("../src/Statistics.js")
+var statistics = require("../src/Statistics.js")
 
-describe("FindMinimumValue", function () {
+describe("Statistics", function () {
 
-	it("see if it works", function () {
-		expect(true).toBe(true);
+	it ("should show minimum, maximum and average value equals 7 and sequence length equals 1 when given [7]", function () {
+		expect(statistics([7])).toEqual("Minimum Value: 7\nMaximum Value: 7\nSequence Length: 1\nAverage Value: 7\n")
 	});
 
-	it ("should return 3 when given 3", function () {
-		expect(findMinimumValue([3])).toEqual(3);
+	it ("should show minimum equals 2, maximum equals 8, average value equals 5 and sequence length equals 2 when given [8,2]", function () {
+		expect(statistics([8,2])).toEqual("Minimum Value: 2\nMaximum Value: 8\nSequence Length: 2\nAverage Value: 5\n")
 	});
 
-	it ("should return 2 when given [4,2,6]", function () {
-		expect(findMinimumValue([4,2,6])).toEqual(2);
-	});
-
-	it ("should return -7 when given [3,18,-7,9]", function () {
-		expect(findMinimumValue([3,18,-7,9])).toEqual(-7);
-	});
-});
-
-describe("FindMaximumValue", function () {
-
-	it("should return 5 when given 5", function () {
-		expect(findMaximumValue([5])).toEqual(5);
+	it ("should show minimum equals -8, maximum equals 15, average value equals 8 and sequence length equals 3 when given [8,-8,15]", function () {
+		expect(statistics([8,-8,15])).toEqual("Minimum Value: -8\nMaximum Value: 15\nSequence Length: 3\nAverage Value: 5\n")
 	});
 
 });
--- Statistics/src/Statistics.js
+++ Statistics/src/Statistics.js
@@ -1,12 +1,27 @@
-function findMinimumValue (sequence) {
+function statistics (sequence) {
+
 	var minimumValue = sequence[0];
-	var seqLength = sequence.length;
-	for (var i = 0; i < seqLength; i++) {
+	var maximumValue = sequence[0];
+	var sequenceLength = sequence.length;
+	var totalValue = 0;
+
+	for (var i = 0; i < sequenceLength; i++) {
+
+		totalValue += sequence[i];
+
 		if (minimumValue > sequence[i]) {
 			minimumValue = sequence[i];
+		} else if (maximumValue < sequence[i]) {
+			maximumValue = sequence[i];
 		}
 	}
-	return minimumValue;
+
+	averageValue = totalValue/sequenceLength;
+
+	return ("Minimum Value: " + minimumValue + "\n" +
+				 "Maximum Value: " + maximumValue + "\n" +
+ 			 	 "Sequence Length: " + sequenceLength + "\n" +
+				 "Average Value: " + averageValue + "\n");
 };
 
-module.exports = findMinimumValue;
+module.exports = statistics;
