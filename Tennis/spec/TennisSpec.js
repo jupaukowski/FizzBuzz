5357459a6fe021edc59a1753992da2b8e1ff67e8
Juliana <jpaukows@thoughtworks.com>
2016-03-07 21:32:22 -0300

    Kata tennis complete?

--- Tennis/spec/TennisSpec.js
+++ Tennis/spec/TennisSpec.js
@@ -1,28 +1,40 @@
-var score = require("../src/Tennis.js")
+var tennis = require("../src/Tennis.js")
 
 describe ("Tennis", function () {
 
-  it("see if it works", function () {
+  it ("see if it works", function () {
     expect(true).toBe(true);
   });
 
-  it("should display the correct score - 0:0 > 0:15", function () {
-		expect(score(0,15)).toEqual("0:15");
+  it ("should display the correct score - 0:0 > 0:15 - Love-Fifteen", function () {
+		expect(tennis([0,0],[0,1])).toEqual("Love-Fifteen");
 	});
 
-  it("should display the correct score - 0:0 > 15:0", function () {
-    expect(score(15,0)).toEqual("15:0");
+  it ("should display the correct score - 0:0 > 15:0 - Fifteen-Love", function () {
+    expect(tennis([0,0],[1,0])).toEqual("Fifteen-Love");
   });
 
-  it("should display Deuce for score 40:40", function () {
-    expect(score(40,40)).toEqual("Deuce");
+  it ("should display the correct score - 15:30 > 30:30 - Thirty-All", function () {
+    expect(tennis([1,2],[1,0])).toEqual("Thirty-All");
   });
 
-  it("should display player 1 as winner if score 40:15", function () {
-    expect(score(40,15)).toEqual("Player 1 wins!");
-  })
+  it ("should display 'Deuce' when the score is 40:40", function () {
+    expect(tennis([2,3],[1,0])).toEqual("Deuce");
+  });
 
-  it("should display player 2 as winner if score 15:40", function () {
-    expect(score(15,40)).toEqual("Player 2 wins!");
-  })
+  it ("should display 'Player 2 wins!' when the score is 0:40", function () {
+    expect(tennis([0,2],[0,1])).toEqual("Player 2 wins!");
+  });
+
+  it ("should display 'Player 1 wins!' when the score is 40:15", function () {
+    expect(tennis([2,1],[1,0])).toEqual("Player 1 wins!");
+  });
+
+  it ("should display 'Advantage for Player 1!' when the score is 40:30", function () {
+    expect(tennis([2,2],[1,0])).toEqual("Advantage for Player 1!");
+  });
+
+  it ("should display 'Advantage for Player 2!' when the score is 40:40 and player 2 scores", function () {
+    expect(tennis([3,3],[0,1])).toEqual("Advantage for Player 2!");
+  });
 });
--- Tennis/src/Tennis.js
+++ Tennis/src/Tennis.js
@@ -1,14 +1,44 @@
-function score (player1, player2) {
+var tennis = function (oldScore, point) {
 
-  if (player1 == 40 && player1 == player2) {
-    return "Deuce";
-  } else if (player1 == 40 && player2 <= 15) {
-    return "Player 1 wins!";
-  } else if (player2 == 40 && player1 <= 15) {
-    return "Player 2 wins!";
+  var namePts = [];
+  var score = [];
+
+  for (var i = 0; i < oldScore.length; i++){
+   score[i] = oldScore[i] + point[i];
+  }
+
+  for (var i = 0; i < oldScore.length; i++) {
+    if (score[i] == 0) {
+      namePts[i] = "Love";
+    } else if (score[i] == 1) {
+      namePts[i] = "Fifteen";
+    } else if (score[i] == 2) {
+      namePts[i] = "Thirty";
+    } else if (score[i] == 3) {
+      namePts[i] = "Forty";
+    }
+  }
+
+
+  if (score[0] >= 3 || score [1] >= 3) {
+    if ((score[0] - score[1]) >= 2) {
+      return "Player 1 wins!";
+    } else if (score[0] == score[1]) {
+      return "Deuce";
+    } else if ((score[1] - score[0]) >= 2) {
+      return "Player 2 wins!";
+    } else if ((score[0] - score[1]) == 1) {
+      return "Advantage for Player 1!";
+    } else if ((score[1] - score[0]) == 1) {
+      return "Advantage for Player 2!";
+    }
   } else {
-    return player1 + ":" + player2;
+    if (namePts[0] == namePts[1]) {
+      return namePts[0] + "-All";
+    } else {
+    return namePts[0] + "-" + namePts[1];
+    }
   }
 };
 
-module.exports = score;
+module.exports = tennis;
